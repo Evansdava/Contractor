8daab562255c1408dbfdbf48a20800b048ca8545
David Evans <evansdav.a@gmail.com>
2019-10-09 11:50:08 -0700

    Users can edit listings

--- app.py
+++ app.py
@@ -5,42 +5,64 @@ import os
 
 app = Flask(__name__)
 
-host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/my_app_db')
+host = os.environ.get("MONGODB_URI", "mongodb://localhost:27017/my_app_db")
 client = MongoClient(host=f"{host}?retryWrites=false")
 db = client.get_default_database()
 listings = db.listings
 
 
-@app.route('/')
+@app.route("/")
 def index():
     """Return homepage"""
-    return render_template('index.html', listings=listings.find())
+    return render_template("index.html", listings=listings.find())
 
 
-@app.route('/new')
+@app.route("/new")
 def new_listing():
     """Return new listing creation page"""
-    return render_template('new_listing.html')
+    return render_template("new_listing.html")
 
 
-@app.route('/new', methods=['POST'])
+@app.route("/new", methods=["POST"])
 def create_listing():
     """Allow the user to create a new listing"""
     listing = {
-        'name': request.form.get('name'),
+        "name": request.form.get("name"),
+        "price": request.form.get("price"),
+        "image": request.form.get("image")
+    }
+    listing_id = listings.insert_one(listing).inserted_id
+    return redirect(url_for("listings_show", listing_id=listing_id))
+
+
+@app.route("/listings/<listing_id>")
+def listings_show(listing_id):
+    """Show a single listing."""
+    listing = listings.find_one({"_id": ObjectId(listing_id)})
+    return render_template("listings_show.html", listing=listing)
+
+
+@app.route('/listings/<listing_id>', methods=['POST'])
+def listings_update(listing_id):
+    """Submit an edited listing."""
+    updated_listing = {
+        'title': request.form.get('title'),
         'price': request.form.get('price'),
         'image': request.form.get('image')
     }
-    listing_id = listings.insert_one(listing).inserted_id
+    listings.update_one(
+        {'_id': ObjectId(listing_id)},
+        {'$set': updated_listing})
     return redirect(url_for('listings_show', listing_id=listing_id))
 
 
-@app.route('/listings/<listing_id>')
-def listings_show(listing_id):
-    """Show a single listing."""
-    listing = listings.find_one({'_id': ObjectId(listing_id)})
-    return render_template('listings_show.html', listing=listing)
+@app.route("/listings/<listing_id>/edit")
+def listings_edit(listing_id):
+    """Show the edit form for a listing."""
+    listing = listings.find_one({"_id": ObjectId(listing_id)})
+    return render_template("listings_edit.html", listing=listing,
+                           title="Edit listing")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
